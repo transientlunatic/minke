9eee9234bf1e54de0d0a1d4d3876e5d24de0452b
Daniel Williams <daniel.williams@ligo.org>
2016-04-26 09:30:44 +0100

    Tried removing the C destructor calls, as this seems not to be helping.

--- minke/sources.py
+++ minke/sources.py
@@ -41,18 +41,6 @@ class Waveform(object):
     numrel_data = []
     waveform = "Generic"
     expnum = 1
-
-    def __del__(self):
-        try:
-            lal.DestroyREAL8Sequence(self.hp)
-            lal.DestroyREAL8Sequence(self.hx)
-        except:
-            print "Failed to destroy hp and hx"
-        try:
-            lal.DestroyREAL8Sequence(self.hp0)
-            lal.DestroyREAL8Sequence(self.hx0)
-        except:
-            print "Failed to destroy hp0 and hx0"
             
     def _clear_params(self):
         self.params = {}
@@ -148,7 +136,7 @@ class Waveform(object):
         else:
             hp0, hx0 = hp, hx
         self.hp, self.hx, self.hp0, self.hx0 = hp, hx, hp0, hx0
-        lalburst.DestroySimBurst(self.swig_row)
+        #lalburst.DestroySimBurst(self.swig_row)
 
     def _row(self, sim=None, slide_id=1):
         """
