0c051407e8c80a43c2457dcc18600efe1dece9b1
Daniel Williams <mail@daniel-williams.co.uk>
2018-03-27 22:45:50 +0100

    Added new version method using git scm.

--- minke/__init__.py
+++ minke/__init__.py
@@ -2,4 +2,10 @@
 
 __author__ = 'Daniel Williams'
 __email__ = 'd.williams.2@research.gla.ac.uk'
-__version__ = '1.0.1'
+
+from pkg_resources import get_distribution, DistributionNotFound
+try:
+    __version__ = get_distribution(__name__).version
+except DistributionNotFound:
+    # package is not installed
+    pass
